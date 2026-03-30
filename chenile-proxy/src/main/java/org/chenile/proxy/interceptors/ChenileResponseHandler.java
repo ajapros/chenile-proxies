4a6d4bd019fcab0e406080a1246a7c3605e31fae
Gaurav Bhardwaj <gauravbhardwaj@VYMO-IT-LPT-730.local>
2026-03-30 20:52:32 +0530

    Add logs

--- chenile-proxy/src/main/java/org/chenile/proxy/interceptors/ChenileResponseHandler.java
+++ chenile-proxy/src/main/java/org/chenile/proxy/interceptors/ChenileResponseHandler.java
@@ -9,6 +9,8 @@ import org.chenile.base.exception.ServerException;
 import org.chenile.base.response.GenericResponse;
 import org.chenile.core.context.ChenileExchange;
 import org.chenile.service.registry.context.RemoteChenileExchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
@@ -20,6 +22,7 @@ import tools.jackson.databind.ObjectMapper;
  * This communicates with SpringRestTemplate to find the errors from the returned response.
  */
 public class ChenileResponseHandler extends DefaultResponseErrorHandler{
+	private static final Logger log = LoggerFactory.getLogger(ChenileResponseHandler.class);
 
 	private final RemoteChenileExchange chenileExchange;
 	private final ObjectMapper objectMapper;
@@ -38,10 +41,13 @@ public class ChenileResponseHandler extends DefaultResponseErrorHandler{
 		HttpStatus statusCode = HttpStatus.resolve(response.getStatusCode().value());
 		if (statusCode != null) {
 			byte[] body = getResponseBody(response);
+			log.warn("Remote service {} returned {} {} for {} {}", getEndpointName(),
+					response.getStatusCode().value(), response.getStatusText(), method, url);
 			if (body.length == 0) {
 				RuntimeException e1 = new ServerException("Error happened in invoking " + getEndpointName() +
 		            response.getStatusCode().value() + response.getStatusText() + " "  +
 						response.getBody());
+				log.warn("Remote service {} returned an empty error body for {} {}", getEndpointName(), method, url);
 				chenileExchange.setException(e1);
 				return;
 			}
@@ -52,15 +58,17 @@ public class ChenileResponseHandler extends DefaultResponseErrorHandler{
 	
 	protected void parseBody(byte[] body,ClientHttpResponse response) throws IOException{
 		try {
-
 			GenericResponse<?> gr = (GenericResponse<?>) objectMapper.readValue(body, getTypeReference());
 			ErrorNumException e1 = new ErrorNumException(gr.getCode(),gr.getSubErrorCode(),
 					gr.getDescription());
+			log.warn("Remote service {} returned business error code={} subErrorCode={}",
+					getEndpointName(), gr.getCode(), gr.getSubErrorCode());
 			chenileExchange.setException(e1);
 			return;
 		}catch(Exception e) {
 			ErrorNumException e1 = new ServerException("Error happened in invoking " + getEndpointName() +
 		            response.getStatusCode().value() + response.getStatusText() );
+			log.error("Failed to parse error response for {}", getEndpointName(), e);
 			chenileExchange.setException(e1);
 		}
 	}
@@ -79,4 +87,3 @@ public class ChenileResponseHandler extends DefaultResponseErrorHandler{
 				chenileExchange.remoteOperationDefinition.name;
 	}	
 }
-
