c349db1766e4d61448475fe98abd70abe6a17768
Gaurav Bhardwaj <gauravbhardwaj@VYMO-IT-LPT-730.local>
2026-05-27 19:16:26 +0530

    Sub erro fixes

--- chenile-proxy/src/main/java/org/chenile/proxy/errorcodes/ErrorCodes.java
+++ chenile-proxy/src/main/java/org/chenile/proxy/errorcodes/ErrorCodes.java
@@ -5,14 +5,14 @@ package org.chenile.proxy.errorcodes;
  */
 public enum ErrorCodes {
 	
-	CANNOT_CONNECT(650), CANNOT_INVOKE(651), MISSING_BODY(652),
+	CANNOT_CONNECT("650"), CANNOT_INVOKE("651"), MISSING_BODY("652"),
 	;
-	final int subError;
-	private ErrorCodes(int subError) {
+	final String subError;
+	private ErrorCodes(String subError) {
 		this.subError = subError;
 	}
 	
-	public int getSubError() {
+	public String getSubError() {
 		return this.subError;
 	}
 }
